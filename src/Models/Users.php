3a309099b65ae28cecdbfc1d7398a2da36878b78
JoseChan <524233828@qq.com>
2022-01-28 11:40:15 +0800

    增加注释

--- src/Models/Users.php
+++ src/Models/Users.php
@@ -11,6 +11,15 @@ namespace JoseChan\UserLogin\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Users
+ * @package JoseChan\UserLogin\Models
+ * @property int $id
+ * @property string $username
+ * @property string $password
+ * @property string $union_id
+ * @property string $openid
+ */
 class Users extends Model
 {
 
